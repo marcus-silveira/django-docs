d4c04bb40d789bd1f3c4df3bb41f4e48c690b990
e.marcus.silveira <e.marcus.silveira@brde.com.br>
2024-02-15 13:00:17 -0300

    finalizado parte 4 dos exemplos

--- webSite/settings.py
+++ webSite/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
